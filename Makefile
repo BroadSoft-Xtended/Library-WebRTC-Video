6c2f6a053406daabae14b4e6d0457072638a24cf
Dominik Steiner <dominik.j.steiner@gmail.com>
2015-05-21 09:46:48 -0600

    add videobg image, set video streams on iceconnection events

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-all: js/templates.js js/styles.js
+all: js/templates.js js/styles.js js/images.js
 
 include ./node_modules/webrtc-core/makefile.defs
--- js/templates.js
+++ js/templates.js
@@ -16,7 +16,7 @@
 
     // video.jade compiled template
     templatizer["video"] = function tmpl_video() {
-        return '<div class="bdsft-client"><div class="video classes"><span class="icon-facetime-video watermark-icon"></span><video autoplay="autoplay" class="remote"></video><div class="localVideo fadeable"><div class="inner"><video autoplay="autoplay" muted="true" class="local"></video></div></div></div></div>';
+        return '<div class="bdsft-client"><div class="video classes"><video autoplay="autoplay" class="remote"></video><div class="localVideo fadeable"><div class="inner"><video autoplay="autoplay" muted="true" class="local"></video></div></div></div></div>';
     };
 
     return templatizer;
